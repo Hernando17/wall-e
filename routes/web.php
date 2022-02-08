d836c260bfe242b0cb99d21e5ad57154cf5d3f0d
Hernando17 <ginkotsu17@gmail.com>
2022-02-08 11:56:25 +0700

    update view

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'index')->name('index');
+Route::view('/detail', 'detail')->name('detail');
